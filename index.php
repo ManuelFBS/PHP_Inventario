466e6150b2cc6f010e0a02dcaac25ddccd21c999
ManuelFBS <manuelf.borrego@gmail.com>
2026-05-02 15:25:45 -0400

    Creac. 'bulma.min.css'; Modif. 'index.php'...

--- index.php
+++ index.php
@@ -1,12 +1,13 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="es" style="background:#fff !important;">
         <head>
                 <meta charset="UTF-8">
                 <meta name="viewport" content="width=device-width, initial-scale=1.0">
                 <title>Sistema de Inventario</title>
+                <link rel="stylesheet" href="./css/bulma.min.css">
         </head>
-        
-        <body>
+
+        <body style="background:#fff !important; color:#000 !important;">
                 <!-- <h1>*************</h1> -->
         </body>
 </html>
