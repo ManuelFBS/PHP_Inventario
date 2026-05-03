b84faf60dbabcd07f7bdf2c5a6a3704fce07d0e9
ManuelFBS <manuelf.borrego@gmail.com>
2026-05-03 15:36:21 -0400

    Modific. 'estilos.css' y 'login.php'...

--- views/login.php
+++ views/login.php
@@ -1,16 +1,17 @@
 <div class="main-container">
-        <form class="box login" action="" method="POST" autocomplete="off">
+
+        <form id="login-form" class="box" action="" method="POST" autocomplete="off">
                 <h5 class="title is-5 has-text-centered is-uppercase">Sistema de Inventario</h5>
 
                 <div class="field">
                         <label class="label">Usuario</label>
                         <div class="control">
-                                <input 
-                                        class="input" 
-                                        type="text" 
-                                        name="login_usuario" 
-                                        pattern="[a-zA-Z0-9]{4,20}" 
-                                        maxlength="20" 
+                                <input
+                                        class="input"
+                                        type="text"
+                                        name="login_usuario"
+                                        pattern="[a-zA-Z0-9]{4,20}"
+                                        maxlength="20"
                                         required
                                 >
                         </div>
@@ -19,12 +20,12 @@
                 <div class="field">
                         <label class="label">Clave</label>
                         <div class="control">
-                                <input 
-                                        class="input" 
-                                        type="password" 
-                                        name="login_clave" 
-                                        pattern="[a-zA-Z0-9$@.-]{7,100}" 
-                                        maxlength="100" 
+                                <input
+                                        class="input"
+                                        type="password"
+                                        name="login_clave"
+                                        pattern="[a-zA-Z0-9$@.-]{7,100}"
+                                        maxlength="100"
                                         required
                                 >
                         </div>
@@ -34,4 +35,5 @@
                         <button type="submit" class="button is-info is-rounded">Iniciar sesión</button>
                 </p>
         </form>
+
 </div>
