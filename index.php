e63f414f1114591bd038f03ee0407c3592998343
ManuelFBS <manuelf.borrego@gmail.com>
2026-05-03 11:02:24 -0400

    Creac. 'session_start.php'; Modif. 'index.php'...

--- index.php
+++ index.php
@@ -1,11 +1,14 @@
+<?php
+require './inc/session_start.php';
+// echo 'ID de sesión: ' . session_id();  // Para depurar
+?>
+
 <!DOCTYPE html>
-<!-- <html lang="es" style="background:#fff !important;"> -->
 <html lang="es" data-theme="light">
         <head>
                 <?php include './inc/head.php' ?>
         </head>
 
-        <!-- <body style="background:#fff !important; color:#000 !important;"> -->
         <body>
                 <?php include './inc/navbar.php' ?>
 
