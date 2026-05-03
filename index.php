c951fdd0857710630524558d7c8bcd5b7dc0b912
ManuelFBS <manuelf.borrego@gmail.com>
2026-05-02 21:50:11 -0400

    Modif. 'index.php', 'estilos.css' y 'navbar.php'; Creac. 'logo.png'...

--- index.php
+++ index.php
@@ -1,10 +1,12 @@
 <!DOCTYPE html>
-<html lang="es" style="background:#fff !important;">
+<!-- <html lang="es" style="background:#fff !important;"> -->
+<html lang="es" data-theme="light">
         <head>
                 <?php include './inc/head.php' ?>
         </head>
 
-        <body style="background:#fff !important; color:#000 !important;">
+        <!-- <body style="background:#fff !important; color:#000 !important;"> -->
+        <body>
                 <?php include './inc/navbar.php' ?>
         </body>
 </html>
