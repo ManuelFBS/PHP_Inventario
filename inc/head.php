5b1070e770d1fb5add9e9deba5a87059b185fbba
ManuelFBS <manuelf.borrego@gmail.com>
2026-05-03 15:49:32 -0400

    Actualiz. 'head.php'...

--- inc/head.php
+++ inc/head.php
@@ -13,5 +13,5 @@ $estilosVersion = @filemtime(__DIR__ . '/../css/estilos.css') ?: time();
 <meta charset="UTF-8">
 <meta name="viewport" content="width=device-width, initial-scale=1.0">
 <title>Sistema de Inventario</title>
-<link rel="stylesheet" href="./css/bulma.min.css">
-<link rel="stylesheet" href="./css/estilos.css">
+<link rel="stylesheet" href="./css/bulma.min.css?v=<?php echo (int) $bulmaVersion; ?>">
+<link rel="stylesheet" href="./css/estilos.css?v=<?php echo (int) $estilosVersion; ?>">
