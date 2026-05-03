d6cd00a05d3e8ba527263d9b33871d63cff8e0c1
ManuelFBS <manuelf.borrego@gmail.com>
2026-05-03 15:46:45 -0400

    Actualiz. 'head.php'...

--- inc/head.php
+++ inc/head.php
@@ -1,3 +1,15 @@
+<?php
+
+/**
+ * * Para evitar problemas de caché: agregue una cadena de consulta de versión basada en la
+ * * hora de la última modificación del archivo.
+ * * Al guardar el archivo CSS, la marca de tiempo cambia y los navegadores descargan el
+ * * nuevo archivo.
+ */
+$bulmaVersion = @filemtime(__DIR__ . '/../css/bulma.min.css') ?: time();
+$estilosVersion = @filemtime(__DIR__ . '/../css/estilos.css') ?: time();
+
+?>
 <meta charset="UTF-8">
 <meta name="viewport" content="width=device-width, initial-scale=1.0">
 <title>Sistema de Inventario</title>
