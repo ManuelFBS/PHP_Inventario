8d3ed5bd29885c732ce5668e2de0689260ebf585
ManuelFBS <manuelf.borrego@gmail.com>
2026-05-11 01:34:19 -0400

    Actualiz. 'user_update.php' y 'user_delete.php'...

--- php/user_delete.php
+++ php/user_delete.php
@@ -1,152 +1,65 @@
-<div class="container is-fluid mb-6">
-	<h1 class="title">Usuarios</h1>
-	<h2 class="subtitle">Actualizar usuario</h2>
-</div>
+<?php
 
-<div class="container pb-6 pt-6">
-        <p class="has-text-right pt-4 pb-4">
-		<a href="#" class="button is-link is-rounded btn-back"><- Regresar atrás</a>
-	</p>
-	<script type="text/javascript">
-                let btn_back = document.querySelector(".btn-back");
+$user_id_del = clean_String($_GET['user_id_del']);
 
-                btn_back.addEventListener('click', function(e){
-                        e.preventDefault();
-                        window.history.back();
-                });
-	</script>
+// * Verificar usuario...
+$db = connect();
 
-        <div class="form-rest mb-6 mt-6"></div>
+$queryUser = "SELECT usuario_id FROM usuario WHERE usuario_id = $user_id_del";
+$check_user = $db->prepare($queryUser);
+$check_user->execute();
 
-        <form action="" method="POST" class="FormularioAjax" autocomplete="off" >
-                <input type="hidden" name="usuario_id" required >
-		
-		<div class="columns">
-                        <div class="column">
-                                <div class="control">
-                                        <label>Nombres</label>
-                                        <input 
-                                                class="input" 
-                                                type="text" 
-                                                name="usuario_nombre" 
-                                                pattern="[a-zA-ZáéíóúÁÉÍÓÚñÑ ]{3,40}" 
-                                                maxlength="40" required 
-                                        >
+if ($check_user->rowCount() == 1) {
+        // > Verificando si el usuario tiene productos registrados...
+        $prodDB = connect();
+        $queryProduct = "SELECT usuario_id FROM producto WHERE usuario_id = $user_id_del LIMIT 1";
+        $check_product = $prodDB->prepare($queryProduct);
+        $check_product->execute();
+
+        if ($check_product->rowCount() <= 0) {
+                $userDB = connect();
+                $queryDeleteUser = 'DELETE FROM usuario WHERE usuario_id = :id';
+                $deleteUser = $userDB->prepare($queryDeleteUser);
+                $deleteUser->execute([':id' => $user_id_del]);
+
+                if ($deleteUser->rowCount() == 1) {
+                        echo '
+                                <div class="notification is-info is-light">
+                                        <strong>Usuario eliminado !</strong><br>
+                                        los datos del usuario se eliminaron con éxito !!!
                                 </div>
-                        </div>
-                        <div class="column">
-                                <div class="control">
-                                        <label>Apellidos</label>
-                                        <input 
-                                                class="input" 
-                                                type="text" 
-                                                name="usuario_apellido" 
-                                                pattern="[a-zA-ZáéíóúÁÉÍÓÚñÑ ]{3,40}" 
-                                                maxlength="40" required 
-                                        >
+                        ';
+                } else {
+                        echo '
+                                <div class="notification is-danger is-light">
+                                        <strong>Ocurrió un error inesperado!</strong><br>
+                                        NO se pudo eliminar el usuario, por favor intente nuevamente...!
                                 </div>
+                        ';
+                }
+                $userDB = null;
+        } else {
+                echo '
+                        <div class="notification is-danger is-light">
+                                <strong>Ocurrió un error inesperado!</strong><br>
+                                NO se puede eliminar el usuario ya que posee productos registrados...!
                         </div>
+                ';
+                // exit();
+        }
+        $check_product = null;
+} else {
+        echo '
+                <div class="notification is-danger is-light">
+                        <strong>Ocurrió un error inesperado!</strong><br>
+                        El usuario que intenta eliminar NO existe...!
                 </div>
+        ';
+        // exit();
+}
+$check_user = null;
 
-                <div class="columns">
-                        <div class="column">
-                                <div class="control">
-                                        <label>Usuario</label>
-                                        <input 
-                                                class="input" 
-                                                type="text" 
-                                                name="usuario_usuario" 
-                                                pattern="[a-zA-Z0-9]{4,20}" 
-                                                maxlength="20" 
-                                                required 
-                                        >
-                                </div>
-                        </div>
-                        <div class="column">
-                                <div class="control">
-                                        <label>Email</label>
-                                        <input 
-                                                class="input" 
-                                                type="email" 
-                                                name="usuario_email" 
-                                                maxlength="70" 
-                                        >
-                                </div>
-                        </div>
-                </div>
+// $query="DELETE FROM usuario WHERE id = ";
+// $stmt=$db->prepare()
 
-                <br><br>
-		<p class="has-text-centered">
-			SI desea actualizar la clave de este usuario por favor llene los 2 campos. Si NO desea actualizar la clave deje los campos vacíos.
-		</p>		
-                <br>
-
-                <div class="columns">
-                        <div class="column">
-                                <div class="control">
-                                        <label>Clave</label>
-                                        <input 
-                                                class="input" 
-                                                type="password" 
-                                                name="usuario_clave_1" 
-                                                pattern="[a-zA-Z0-9$@.-]{7,100}" 
-                                                maxlength="100" 
-                                        >
-                                </div>
-                        </div>
-                        <div class="column">
-                                <div class="control">
-                                        <label>Repetir Clave</label>
-                                        <input 
-                                                class="input" 
-                                                type="password" 
-                                                name="usuario_clave_2" 
-                                                pattern="[a-zA-Z0-9$@.-]{7,100}" 
-                                                maxlength="100" 
-                                        >
-                                </div>
-                        </div>
-                </div>
-
-                <br><br><br>
-		<p class="has-text-centered">
-			Para poder actualizar los datos de este usuario por favor ingrese su USUARIO y CLAVE con la que ha iniciado sesión
-		</p>
-                <div class="columns">
-                        <div class="column">
-                                <div class="control">
-                                        <label>Usuario</label>
-                                        <input 
-                                                class="input" 
-                                                type="text" 
-                                                name="administrador_usuario" 
-                                                pattern="[a-zA-Z0-9]{4,20}" 
-                                                maxlength="20" required 
-                                        >
-                                </div>
-                        </div>
-                        <div class="column">
-                                <div class="control">
-                                        <label>Clave</label>
-                                        <input 
-                                                class="input" 
-                                                type="password" 
-                                                name="administrador_clave" 
-                                                pattern="[a-zA-Z0-9$@.-]{7,100}" 
-                                                maxlength="100" 
-                                                required 
-                                        >
-                                </div>
-                        </div>
-                </div>
-
-                <p class="has-text-centered">
-			<button type="submit" class="button is-success is-rounded">Actualizar</button>
-		</p>
-        </form>
-
-        <div class="notification is-danger is-light mb-6 mt-6">
-	        <strong>¡Ocurrio un error inesperado!</strong><br>
-	        No podemos obtener la información solicitada...!
-	</div>
-</div>
+?>
