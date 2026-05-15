0aebc0965edc1eb4635a74c4f3f65abb78c4ca37
ManuelFBS <manuelf.borrego@gmail.com>
2026-05-15 13:01:24 -0400

    Creac. 'show_category_list.php' y 'category_delete.php'; Modif. 'category_list.php'...

--- views/category_list.php
+++ views/category_list.php
@@ -1,1 +1,15 @@
-Lista de Categorías...
+<div class="container is-fluid mb-6">
+        <h1 class="title">Categorías</h1>
+        <h2 class="subtitle">Lista de categorías</h2>
+</div>
+
+<div class="container pb-6 pt-6">
+        <?php
+        require_once './php/main.php';
+
+        // * Eliminar categoría...
+        if (isset($_POST['category_id_del'])) {
+                require_once '';
+        }
+        ?>
+</div>
