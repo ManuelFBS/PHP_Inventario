2c70dd49064150ffd399d75e9058d4bdb71f422b
ManuelFBS <manuelf.borrego@gmail.com>
2026-05-20 14:27:19 -0400

    Actualiz. 'navbar.php' y 'product_new.php'...

--- inc/navbar.php
+++ inc/navbar.php
@@ -35,10 +35,10 @@
                         <div class="navbar-item has-dropdown is-hoverable">
                                 <a class="navbar-link">Productos</a>
                                 <div class="navbar-dropdown">
-                                        <a class="navbar-item">Nuevo</a>
-                                        <a class="navbar-item">Lista</a>
+                                        <a class="navbar-item" href="index.php?view=product_new">Nuevo</a>
+                                        <a class="navbar-item" href="index.php?view=product_list">Lista</a>
                                         <a class="navbar-item">Por categoría</a>
-                                        <a class="navbar-item">Buscar</a>
+                                        <a class="navbar-item" href="index.php?view=product_search">Buscar</a>
                                 </div>
                         </div>
                 </div>
--- views/product_new.php
+++ views/product_new.php
@@ -1,1 +1,25 @@
-Producto Nuevo...
+<div class="container is-fluid mb-6">
+	<h1 class="title">Productos</h1>
+	<h2 class="subtitle">Nuevo producto</h2>
+</div>
+
+<div class="container pb-6 pt-6">
+        <div class="form-rest mb-6 mt-6"></div>
+
+        <form action="./php/product_save.php" method="POST"  class="FormAjax" autocomplete="off">
+                <div class="columns">
+		  	<div class="column">
+                                <div class="control">
+                                        <label>Código</label>
+                                        <input class="input" type="text" name="producto_codigo" pattern="[a-zA-ZáéíóúÁÉÍÓÚñÑ ]{3,40}" maxlength="40" required >
+                                </div>
+                        </div>
+		  	<div class="column">
+		    	        <div class="control">
+					<label>Nombre</label>
+				  	<input class="input" type="text" name="producto_nombre" pattern="[a-zA-ZáéíóúÁÉÍÓÚñÑ ]{3,40}" maxlength="40" required >
+				</div>
+		  	</div>
+		</div>
+        </form>
+</div>
